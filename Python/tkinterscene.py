582e9ae9132e175e54e4e64f0eb59455382696a4
Tommy Shoemaker <thshoemaker234@gmail.com>
2019-03-25 13:01:44 -0700

    tinkerscene, photoshop, image changes

--- Python/tkinterscene.py
+++ Python/tkinterscene.py
@@ -11,10 +11,11 @@ canvas.pack()
 hexred = "#ff0000"
 hexgreen = "#00ff00"
 hexblue = "#0000ff"
-
+student = PhotoImage(file="computer.gif")
 #Code part:
 
 while 1:
+    picX = random.randint(1,400)
     r = lambda: random.randint(0,255)
     hexcolor = '#%02X%02X%02X' % (r(),r(),r())
     x1 = random.randint(0,400)
@@ -23,5 +24,6 @@ while 1:
     y2 = y1 + 100
     canvas.create_rectangle(0,0,450,450,fill="black",outline="")
     canvas.create_oval(x1,y1,x2,y2,fill=hexcolor,outline="")
+    canvas.create_image(picX,200,image=student)
     tk.update()
     time.sleep(0.5)
